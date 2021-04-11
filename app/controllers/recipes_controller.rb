53e3bdf35e42d4fb2afb94310989f50d15a08f70
tano-taka <tanowaki.takamasa@gmail.com>
2021-04-11 12:55:14 +0900

    詳細,編集画面の実装

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -17,6 +17,25 @@ class RecipesController < ApplicationController
     end
   end
 
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+    redirect_to recipe_path(@recipe.id)
+    else
+      render :edit
+    end
+  end
+
+
+
 
   private
 
